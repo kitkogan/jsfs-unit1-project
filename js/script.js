b6ceb4b6403a325627a391730bfb927a1da3bc9f
Kit Kogan <kitkogan@users.noreply.github.com>
2019-07-27 00:07:01 -0500

    Some edits
    Added a bunch of background colors that I liked to js file;
    added time interval and clear timer functions;
    Added if/else commands to the program;
    Checked some of the variables;
    Changed some of the html and css information

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ var quotes = [
   {
 
     quote: "Art is Skill; its display, application, or expression.",
-    source: "Oxford English Dictionary Online.",
+    source: "- Oxford English Dictionary Online.",
     citation: "published again for Mental Floss 'What is Art?' in 2014.",
     year: "1300s.",
     tags: "#dictionarysays"
@@ -29,7 +29,7 @@ var quotes = [
   {
 
     quote: "The expression or application of creative skill and imagination, typically in a visual form such as painting, drawing, or sculpture, producing works to be appreciated primarily for their beauty or emotional power.",
-    source: "Oxford English Dictionary Online.",
+    source: "- Oxford English Dictionary Online.",
     citation: "published again for Mental Floss 'What is Art?' in 2014",
     year: "1600s.",
     tags: "#dictionarysays"
@@ -38,7 +38,7 @@ var quotes = [
   {
 
     quote: "Art is either a plagiarist or a revolutionary.",
-    source: "Paul Gauguin (1848–1903), Peruvian-born French artist",
+    source: "- Paul Gauguin (1848–1903), Peruvian-born French artist",
     citation: "quoted in Huneker, The Pathos of Distance",
     year: 1913,
     tags: "#imitationVScreation"
@@ -47,7 +47,7 @@ var quotes = [
   {
 
     quote: "Filling a space in a beautiful way. That's what art means to me.",
-    source: "Georgia O'Keeffe (1887–1986), American painter",
+    source: "- Georgia O'Keeffe (1887–1986), American painter",
     citation: "Art News December 1977",
     year: 1977,
     tags: "#beauty"
@@ -56,7 +56,7 @@ var quotes = [
   {
 
     quote: "We all know that Art is not truth. Art is a lie that makes us realize truth.",
-    source: "Pablo Picasso (1881–1973), Spanish painter living in France",
+    source: "- Pablo Picasso (1881–1973), Spanish painter living in France",
     citation: "quoted in Dore Ashton's 'Picasso on Art'",
     year: 1972,
     tags: "#revelation"
@@ -65,7 +65,7 @@ var quotes = [
   {
 
     quote: "[In order to distinguish Andy Warhol’s Brillo Boxes from actual Brillo boxes, art can be defined as] embodied meaning.",
-    source: "Arthur C. Danto (1924–2013), American philosopher of art",
+    source: "- Arthur C. Danto (1924–2013), American philosopher of art",
     citation: "What Art Is",
     year: 2013,
     tags: "#imitationVScreation"
@@ -74,7 +74,7 @@ var quotes = [
   {
 
     quote: "What is art? Art grows out of grief and joy, but mainly grief. It is born of people’s lives.",
-    source: "Edvard Munch (1863–1944), Norwegian artist",
+    source: "- Edvard Munch (1863–1944), Norwegian artist",
     citation: "'Edvard Munch: The Man and His Art' by Ragna Stang",
     year: 1977,
     tags: "#self-expression"
@@ -83,7 +83,7 @@ var quotes = [
   {
 
     quote: "Art is a habit-forming drug.",
-    source: "Marcel Duchamp (1887–1968), French-born American artist",
+    source: "- Marcel Duchamp (1887–1968), French-born American artist",
     citation: "quoted in Richter, 'Dada: art and anti-art'",
     year: 1964,
     tags: "#addiction"
@@ -92,7 +92,7 @@ var quotes = [
   {
 
     quote: "Art does not reproduce the visible; rather, it makes visible.",
-    source: "Paul Klee (1879–1940), Swiss painter",
+    source: "- Paul Klee (1879–1940), Swiss painter",
     citation: "The Inward Vision",
     year: 1959,
     tags: "#revelation"
@@ -102,14 +102,116 @@ var quotes = [
 ];
 
 console.log(quotes);
+/***
+Function below will:
+1)Generate a "random" background color (out of a list of many that I have chosen)
+2)and display in the background as each newly generated quote is refreshed
+***/
 
+var colors = [
+  {
+    backgroundColor: "#00FFFF", // aqua
+  },
+  { 
+    backgroundColor: "#7FFFD4", // aquamarine
+  },
+  {
+    backgroundColor: "#8A2BE2", // blueviolet
+  },
+  {
+    backgroundColor: "#5F9EA0", // cadet blue
+  },
+  {
+    backgroundColor: "#7FFF00", // chartreuse
+  },
+  {
+    backgroundColor: "#FF7F50", // coral
+  },
+  {
+    backgroundColor: "#6495ED", //cornflower blue
+  },
+  {
+    backgroundColor: "#00FFFF", //cyan
+  },
+  {
+    backgroundColor: "#008B8B", // dark cyan
+  },
+  {
+    backgroundColor: "#8B008B", // dark magenta
+  },
+  {
+    backgroundColor: "#9932CC", // dark orchid
+  },
+  {
+    backgroundColor: "#00CED1", // dark turquoise
+  },
+  {
+    backgroundColor: "#9400D3", // dark violet
+  },
+  {
+    backgroundColor: "#FF1493", // deep pink
+  },
+  {
+    backgroundColor: "#00BFFF", // deep sky blue
+  },
+  {
+    backgroundColor: "#FF00FF", // fuchsia
+  },
+  {
+    backgroundColor: "#FFD700", // gold
+  }, 
+  {
+    backgroundColor: "#ADFF2F", // green yellow
+  }, 
+  {
+    backgroundColor: "#FF69B4", // hot pink
+  },
+  {
+    backgroundColor: "#FFB6C1", // light pink
+  },
+  {
+    backgroundColor: "#FF00FF", // magenta
+  },
+  {
+    backgroundColor: "#BA55D3", // medium orchid
+  },
+  {
+    backgroundColor: "#7B68EE", // medium slate blue
+  },
+  {
+    backgroundColor: "#C71585", // medium violet red
+  },
+  {
+    backgroundColor: "#DA70D6", // orchid
+  },
+  {
+    backgroundColor: "#DDA0DD", // plum
+  },
+  {
+    backgroundColor: "#B0E0E6", // powder blue
+  },
+  { 
+    backgroundColor: "#40E0D0", // turquoise
+  },
+  {
+    backgroundColor: "#EE82EE", // violet
+  },
+  {
+    backgroundColor: "#F5F5F5", // white smoke
+  },
+  {
+    backgroundColor: "#9ACD32", // yellow green
+  }
+
+];
 /***
 Function below will: 
 1)Generate a random number ("getRandomQuote") to then apply to "quotes" array,
 2)Choose random quote based on parameters set by program: a random number between 0 - and last number of array index length
 3)Return random quote object from "quotes" array index
-
 ***/
+
+var timer;
 function getRandomQuote(array) { 
   var quoteIndex = Math.floor(Math.random() * (quotes.length)); 
   for (var i = 0; i < array.length; i++) { 
@@ -117,11 +219,46 @@ function getRandomQuote(array) {
   }
   return randomQuote; 
 }
-var result = getRandomQuote(quotes);
-console.log(result);
+var resultQuote = getRandomQuote(quotes);
+console.log(resultQuote);
 
+/*** 
+Function below will:
+1) Generate a random number ("getRandomColor") to then apply to the "rbgColor" array,
+2) Choose a random color based on parameters set by the program: a random number between 0 - and last number of the array index length
+3)Return a random background color object from "colors" array index
+***/
+ 
+function getRandomColor(array) {
+  var colorIndex = Math.floor(Math.random() * (colors.length));
+  for (var i = 0; i < array.length; i++) {
+       var randomColor = array[colorIndex];
+  }
+  return randomColor;
+}
+
+var resultColor = getRandomColor(colors);
 
 /***
+
+Function below will:
+Set an interval timer to variable named above allowing quote to auto-switch
+after 15 seconds.
+***/
+
+function startTimer() {
+  timer = setInterval(printQuote, 15000);
+}
+/*** 
+
+Function below will:
+Clear interval variable timer
+***/
+function clearTimer() {
+  clearInterval(timer);
+}
+/*** 
+ 
 Function below will:
 Allow randomly generated quote and other indicated information to be printed as a "message" as programmed.
   (-Please note: For the purpose of this project "if/else" statements have been included in the code although 
@@ -148,9 +285,9 @@ function printQuote() {
   } else { 
   message += "</p>";
   }
-}
 document.getElementById('quote-box').innerHTML = message;
-printQuote(); 
+}
+printQuote();
 
 /***
   When the "Show another quote" button is clicked, the event listener 
