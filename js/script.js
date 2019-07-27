c4447ea108fc6f582a80a3612f039ee4f1573e85
Kit Kogan <kitkogan@users.noreply.github.com>
2019-07-27 02:34:33 -0500

    Revert to earlier version
    Reverted back to previous verion of this code, as the timer and background randomizer was not working and the "fixes" attmepted were not improving matters.

--- js/script.js
+++ js/script.js
@@ -16,13 +16,15 @@ I am attempting to get an "exceeds expectations" review on this project.
 Thank you!
 ***/
 
+
+
 var quotes = [
   {
 
     quote: "Art is Skill; its display, application, or expression.",
     source: " Oxford English Dictionary Online.",
     citation: "published again for Mental Floss 'What is Art?' in 2014.",
-    year: " 1300s.",
+    year: "1300s.",
     tags: "#dictionarysays"
 
   },
@@ -32,7 +34,7 @@ var quotes = [
     source: " Oxford English Dictionary Online.",
     citation: "published again for Mental Floss 'What is Art?' in 2014",
     year: "1600s.",
-    tags: " #dictionarysays"
+    tags: "#dictionarysays"
 
   },
   {
@@ -41,7 +43,7 @@ var quotes = [
     source: " Paul Gauguin (1848–1903), Peruvian-born French artist",
     citation: "quoted in Huneker, The Pathos of Distance",
     year: 1913,
-    tags: " #imitationVScreation"
+    tags: "#imitationVScreation"
 
   },
   {
@@ -50,7 +52,7 @@ var quotes = [
     source: " Georgia O'Keeffe (1887–1986), American painter",
     citation: "Art News December 1977",
     year: 1977,
-    tags: " #beauty"
+    tags: "#beauty"
 
   },
   {
@@ -59,7 +61,7 @@ var quotes = [
     source: " Pablo Picasso (1881–1973), Spanish painter living in France",
     citation: "quoted in Dore Ashton's 'Picasso on Art'",
     year: 1972,
-    tags: " #revelation"
+    tags: "#revelation"
 
   },
   {
@@ -68,7 +70,7 @@ var quotes = [
     source: " Arthur C. Danto (1924–2013), American philosopher of art",
     citation: "What Art Is",
     year: 2013,
-    tags: " #imitationVScreation"
+    tags: "#imitationVScreation"
 
   },
   {
@@ -77,7 +79,7 @@ var quotes = [
     source: " Edvard Munch (1863–1944), Norwegian artist",
     citation: "'Edvard Munch: The Man and His Art' by Ragna Stang",
     year: 1977,
-    tags: " #self-expression"
+    tags: "#self-expression"
 
   },
   {
@@ -86,7 +88,7 @@ var quotes = [
     source: " Marcel Duchamp (1887–1968), French-born American artist",
     citation: "quoted in Richter, 'Dada: art and anti-art'",
     year: 1964,
-    tags: " #addiction"
+    tags: "#addiction"
 
   },
   {
@@ -95,7 +97,7 @@ var quotes = [
     source: " Paul Klee (1879–1940), Swiss painter",
     citation: "The Inward Vision",
     year: 1959,
-    tags: " #revelation"
+    tags: "#revelation"
 
   }
 
@@ -210,38 +212,45 @@ Function below will:
 2)Choose random quote based on parameters set by program: a random number between 0 - and last number of array index length
 3)Return random quote object from "quotes" array index
 ***/
-function getRandomQuote() {
-  let randomQuote = Math.floor(Math.random() * quotes.length);
 
-  return quotes[randomQuote];
+var timer;
+function getRandomQuote(array) { 
+  var quoteIndex = Math.floor(Math.random() * (quotes.length)); 
+  for (var i = 0; i < array.length; i++) { 
+       var randomQuote = array[quoteIndex]; 
+  }
+  return randomQuote; 
 }
-  console.log(randomQuote);
+var resultQuote = getRandomQuote(quotes);
+console.log(resultQuote);
+
 /*** 
 Function below will:
-1) Generate a random number ("getRandomColor") to then apply to the "rbgColor" array,
+1) Generate a random number ("getRandomColor") to then apply to the "colors" array,
 2) Choose a random color based on parameters set by the program: a random number between 0 - and last number of the array index length
 3)Return a random background color object from "colors" array index
 ***/
  
-function getRandomColor() {
-  let randomColor = Math.floor(Math.random() * colors.length);
-
-  return colors[randomColor];
+function getRandomColor(array) {
+  var colorIndex = Math.floor(Math.random() * (colors.length));
+  for (var i = 0; i < array.length; i++) {
+       var randomColor = array[colorIndex];
+  }
+  return randomColor;
 }
 
+var resultColor = getRandomColor(colors);
 
 /***
-
 Function below will:
-Set an interval timer to variable by allowing quote to auto-switch
+Set an interval timer to variable named above allowing quote to auto-switch
 after 15 seconds.
 ***/
-let timer;
+
 function startTimer() {
   timer = setInterval(printQuote, 15000);
 }
 /*** 
-
 Function below will:
 Clear interval variable timer
 ***/
@@ -258,33 +267,26 @@ Allow randomly generated quote and other indicated information to be printed as 
 ***/
 
 function printQuote() {  
-  let displayQuote = getRandomQuote();
-  let displayColor = getRandomColor();
-  message = "<p class='quote'>" + displayQuote.quote + "</p>";
-  message += "<p class='source'>" + displayQuote.source;
-  if ( displayQuote.citation ) {
-    message += "<span class='citation'>" + displayQuote.citation + "</span>";
+  var result = getRandomQuote(quotes);
+  message = "<p class='quote'>" + result.quote + "</p>";
+  message += "<p class='source'>" + result.source;
+  if ( result.citation ) {
+    message += "<span class='citation'>" + result.citation + "</span>";
   } else {
     message += '';
   } 
-  if ( displayQuote.year ) { 
-    message += "<span class='year'>" + displayQuote.year + "</span>";
+  if ( result.year ) { 
+    message += "<span class='year'>" + result.year + "</span>";
   } else { 
     message += '';
   }
-	if ( displayQuote.tags ) {   
-    message += "<span class='tags'>" + displayQuote.tags + "</span>";
+	if ( result.tags ) {   
+    message += "<span class='tags'>" + result.tags + "</span>";
   } else { 
   message += "</p>";
   }
-
-document.getElementById("quote-box").innerHTML = html;
-document.body.style.background = displayColor.background;
-
-clearTimer();
-startTimer();
+document.getElementById('quote-box').innerHTML = message;
 }
-
 printQuote();
 
 /***
@@ -295,4 +297,3 @@ printQuote();
 ***/
 
 document.getElementById("loadQuote").addEventListener("click", printQuote, false);
-
